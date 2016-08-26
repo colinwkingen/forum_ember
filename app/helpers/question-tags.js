701d8f1adb39c8ef960c4ce21380be891a192ff0
Colin Kingen <colinwkingen@gmail.com>
2016-08-26 11:23:12 -0700

    create helper to add glyphicon tags for serious and nsfw

--- app/helpers/question-tags.js
+++ app/helpers/question-tags.js
@@ -2,8 +2,15 @@ import Ember from 'ember';
 
 export function questionTags(params/*, hash*/) {
   var question = params[0];
+  var tags = "";
+  if (question.get('nsfw') === true ) {
+    tags += "<span class='glyphicon glyphicon-alert'> NSFW</span><br>";
+  }
+  if (question.get('serious') === true ) {
+    tags += "<span class='glyphicon glyphicon-alert'> Serious</span>";
+  }
 
-  return question.get('author');
+  return Ember.String.htmlSafe(tags);
 }
 
 export default Ember.Helper.helper(questionTags);
