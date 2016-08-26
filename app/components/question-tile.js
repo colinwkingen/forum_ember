068a0c8402bea2029bd411c48827436b1c2f0007
Colin Kingen <colinwkingen@gmail.com>
2016-08-26 08:40:03 -0700

    create more varied info  based on number of answers in main page

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,10 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   questionInfo: Ember.computed('question.answers', function() {
-    if (!this.get('question.answers')) {
+    var answerNumber = this.get('question.answers.length');
+    if ( !answerNumber) {
       return "No answers yet!";
+    } else if (answerNumber === 1){
+      return "This only has " + answerNumber.toString() + " answer.";
+    } else if (answerNumber < 5) {
+      return "This currently has " + answerNumber.toString() + " answers.";
     } else {
-      return "Some answers.";
+      return "This question is on fire with " + answerNumber.toString() + " answers.";
     }
   }),
 
