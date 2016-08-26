a86542d5bac4d7f5b6080f207c0a26f08a9f868b
Colin Kingen <colinwkingen@gmail.com>
2016-08-26 15:26:11 -0700

    fixed some styling issues and made it look a bit less brutal

--- app/helpers/question-tags.js
+++ app/helpers/question-tags.js
@@ -2,14 +2,13 @@ import Ember from 'ember';
 
 export function questionTags(params/*, hash*/) {
   var question = params[0];
-  var tags = "";
+  var tags = " ";
   if (question.get('nsfw') === true ) {
-    tags += "<span class='glyphicon glyphicon-alert'> NSFW</span>";
+    tags += " <span class='glyphicon glyphicon-alert'> NSFW</span> ";
   }
   if (question.get('serious') === true ) {
     tags += "<span class='glyphicon glyphicon-alert'> Serious</span>";
   }
-
   return Ember.String.htmlSafe(tags);
 }
 
